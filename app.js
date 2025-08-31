76b112d824c28f6d11e373e4fc4a5aaea72a26a5
User004DotCom <gaminig147896325@gmail.com>
2025-08-30 23:56:31 -0300

    Creacion de la primera funcion

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+    console.log(nombreAmigo);
+
+    listaAmigos.push(nombreAmigo);
+    console.log(listaAmigos);
+
+    limpiarCaja();
+
+    actualizarListaVisual("listaAmigos", listaAmigos)
+    return;
+}
+
+function actualizarListaVisual(idLista, arrayDeNombres) {
+    const ul = document.getElementById(idLista);
+    ul.innerHTML = ""; // Limpia la lista visual
+
+    arrayDeNombres.forEach(nombre => {
+        const li = document.createElement("li");
+        li.textContent = nombre;
+        ul.appendChild(li);
+    });
+}
+
+function limpiarCaja() {
+    document.getElementById("amigo").value = "";
+    return;
+}
